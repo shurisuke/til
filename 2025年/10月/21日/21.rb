475546981c33f5b7937f299d077176c21a908f82
shuri <inoseshuri@icloud.com>
2025-10-21 21:45:54 +0900

    add:今日のまとめとメモの追加

--- 2025年/10月/21日/21.rb
+++ 2025年/10月/21日/21.rb
@@ -1,41 +1,10 @@
-# ① レイアウトファイルであるadmin.html.slimでyield 'scripts'として、scriptsスロットの表示場所を確保＆app/assets/javascripts/admin.jsの読み込み
+# medium.image_url(:lg)のエラー対応の参考コード
 
+.media-image
+    = image_tag medium.image_url(:lg)
 
+## image_url生成時にnilを返してエラーが出ないように、安全な分岐処理に変更
 
-
-# ② edit.html.slimでedit_article.jsの読み込み
-
-
-
-
-# ③ app/javascript/edit_article.jsでapp/javascript/articleBlockEditor/index.jsの読み込み。
-
-
-
-
-# ④ index.jsでモジュールの読み込みと jQuery プラグイン定義、描画対象のDOMを取得、textarea要素を取得、onChangeハンドラ定義、Reactコンポーネントを描画
-
-
-
-
-# ⑤ js-article-block-formsクラスのオブジェクトに、ajaxでurlやmethod、datatypeを埋め込む。これがページがロードされるとすぐs-article-block-formsクラスのオブジェクトに実行されるので、ArticleBlocksController#index が実行
-
-
-
-
-# ⑥ app/controller/admin/articles/article_block_controller#indexアクションが呼ばれ、@article_blocks 変数の用意をビューへ渡す。
-
-
-
-
-# ⑦ views/admin/articles/article_blocks/index.html.slimで、admin/articles/article_blocks/insert_blockの描画と、@article_blocksの要素ごとにadmin/articles/article_blocks/article_blockとadmin/articles/article_blocks/insert_blockの描画を実行
-
-
-
-
-# ⑧ admin/articles/article_blocks/insert_blockでは、ブロックを構築するをクリックした際に、動的なhtml要素をajaxを使って実行
-
-
-
-
-# ⑨ admin/articles/article_blocks/article_blockでは個々の ArticleBlock を箱のように表示する UI を構築。js-article-block-content の中で、それぞれのブロックタイプ（sentence, medium, embed）に応じてパーシャルを切り替えて描画している。
+.media-image
+  - if medium.image_url(:lg).present?
+    = image_tag medium.image_url(:lg)
