87d0f969ba6b9c95a9cc5368303e7de7cccc89c9
shurisuke <inoseshuri@icloud.com>
2025-11-04 23:48:17 +0900

    content_tag の基本と使い所

--- 2025年/11月/4日/4.rb
+++ 2025年/11月/4日/4.rb
@@ -6,8 +6,20 @@
                               :last
 
 
+# ラジオボタンを縦並びにする方法
+
 = f.collection_radio_buttons :eye_catch_send,
                               Article.eye_catch_sends_i18n,
                               :first,
                               :last,
                               item_wrapper_class: "d-block"
+
+
+# content_tag の基本と使い所
+
+content_tag(:iframe, nil,
+  src: "https://www.youtube.com/embed/#{embed.identifier}",
+  width: 560,
+  height: 315,
+  allowfullscreen: true
+)
