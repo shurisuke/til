a76be2b88beaa6c7a96ddf88f3f69d68de6ab189
shurisuke <inoseshuri@icloud.com>
2025-11-04 23:45:02 +0900

    Simple Form での collection_radio_buttons の使い方

---
+++ 2025年/11月/4日/4.rb
@@ -0,0 +1,8 @@
+# Simple Form での collection_radio_buttons の使い方
+
+= f.collection_radio_buttons :eye_catch_send,
+                              Article.eye_catch_sends_i18n,
+                              :first,
+                              :last
+
+
