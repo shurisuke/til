236b278c772a59f8843d2f21c4a4c1f0ce260cea
shurisuke <inoseshuri@icloud.com>
2025-11-05 22:28:18 +0900

    add:Twitter(X)の投稿をAPI不要で埋め込む方法

--- 2025年/11月/5日/5.rb
+++ 2025年/11月/5日/5.rb
@@ -1,3 +1,11 @@
 # YouTubeの埋め込みURL構造について
 ## Slimでの記述例
 iframe width="560" height="315" src="https://www.youtube.com/embed/#{embed.identifier}" frameborder="0" allowfullscreen=true
+
+
+# Twitter(X)の投稿をAPI不要で埋め込む方法
+## Slimでの最小構成例
+.embed-twitter
+  blockquote.twitter-tweet
+    a href="https://twitter.com/ユーザー名/status/ツイートID"
+  script async=true src="https://platform.twitter.com/widgets.js" charset="utf-8"
