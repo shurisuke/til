c6033b82b212dd4241dc4c776a185beb3e9a29c1
shurisuke <inoseshuri@icloud.com>
2025-11-14 17:45:52 +0900

    add:whereとhavingの違い

--- 2025年/11月/14日/14.rb
+++ 2025年/11月/14日/14.rb
@@ -3,3 +3,10 @@
 .group("category.name")
 .having("revenue >= ?", 100)
 
+
+2. whereとhavingの違いを学んだ
+whereで絞る例（OK）：             Payment.where(amount: 5)
+havingで絞る例（OK）：            Category
+                                .group("category.name")
+                                .having("SUM(payment.amount) >= ?", 100)
+whereに集計関数を使うとエラー（NG）：Category.where("SUM(payment.amount) >= ?", 100)
